0fce9a266aa5fa30cd832b99da81e87821af850b
Terrance <terrance.hanlon@ndsu.edu>
2020-06-16 20:06:41 -0500

    add swing animation

--- core/entities/alive/Entity.cpp
+++ core/entities/alive/Entity.cpp
@@ -4,25 +4,44 @@ Entity::Entity(){
     // this->ani = *new AnimatedSprite();
     this->movementSpeed = 1.0f;
     this->movementComp = new Movement(this->movementSpeed);
-    text.loadFromFile("Assets/Sprites/King_Human/Run.png");
+    this->run_text.loadFromFile("Assets/Sprites/King_Human/Run.png");
+    
     // sf::IntRect rectSourceSprite(300, 0, 300, 400);
     // this->sprite = new Sprite(this->text, rectSourceSprite);
-    anim.setSpriteSheet(text);
-    anim.addFrame(sf::IntRect(9, 18, 37, 25));
-    anim.addFrame(sf::IntRect(87, 16, 37, 26));
-    anim.addFrame(sf::IntRect(165, 17, 37, 26));
-    anim.addFrame(sf::IntRect(243, 20, 37, 25));
-    anim.addFrame(sf::IntRect(321, 18, 37, 25));
+    this->anim.setSpriteSheet(this->run_text);
+    this->anim.addFrame(sf::IntRect(9, 18, 37, 25));
+    this->anim.addFrame(sf::IntRect(87, 16, 37, 26));
+    this->anim.addFrame(sf::IntRect(165, 17, 37, 26));
+    this->anim.addFrame(sf::IntRect(243, 20, 37, 25));
+    this->anim.addFrame(sf::IntRect(321, 18, 37, 25));
     // anim.addFrame(sf::IntRect(388, 16, 37, 26)); //this one triggers the off screen
-    anim.addFrame(sf::IntRect(477, 17, 37, 26));
-    anim.addFrame(sf::IntRect(555, 20, 37, 25));
+    this->anim.addFrame(sf::IntRect(477, 17, 37, 26));
+    this->anim.addFrame(sf::IntRect(555, 20, 37, 25));
 
-    ani.setAnimation(this->anim);
-    ani.play(anim);
+    this->attack_text.loadFromFile("Assets/Sprites/King_Human/Attack.png");
+    this->attack.setSpriteSheet(this->attack_text);
+    this->attack.addFrame(sf::IntRect(20, 0, 59, 58));
+    this->attack.addFrame(sf::IntRect(100, 0, 50, 58));
+    this->attack.addFrame(sf::IntRect(165, 16, 37, 28));
+
+    this->ani.setAnimation(this->anim);
+    this->ani.play(this->anim);
+
+    this->current_animation = &this->anim;
     
 };
 
+void Entity::swing(){
+    this->current_animation = &this->attack;
+};
+
+void Entity::run(){
+    // ani.setAnimation(this->anim);
+    this->current_animation = &this->anim;
+
+};
 void Entity::update(sf::Time deltaTime){
+    this->ani.play(*this->current_animation);
     if(this->ani.get_last_hit().asSeconds() >= 0.5){
         this->ani.setColor(sf::Color::White);
     }
--- core/entities/alive/headers/Entity.hpp
+++ core/entities/alive/headers/Entity.hpp
@@ -1,7 +1,12 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+// #pragma once
+
 #include "../../../components/headers/Movement.hpp"
+// #include "../../entities/structural/headers/Platform.hpp"
+// #include "../../structural/headers/Platform.hpp"
+// #include "../../../../Lib/Imports.hpp"
 
 class Entity
 {
@@ -17,13 +22,22 @@ class Entity
         Entity();
         AnimatedSprite ani;
         Animation anim;
-        sf::Texture text;
+        Animation attack;
+        sf::Texture run_text;
+        sf::Texture attack_text;
+        // sf::Texture 
 
         void update(sf::Time deltaTime);
         void render(sf::RenderWindow *window);
 
+        void swing();
+        void run();
+
         int maxHealth = 3;
         int currentHealth = 3;
+
+        Animation *current_animation;
+
 
 
 };
